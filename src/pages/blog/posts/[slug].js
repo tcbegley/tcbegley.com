9b7ec9aaa51c3e86e5a0bf72051f05545331f4f2
tcbegley <tomcbegley@gmail.com>
2022-09-08 23:12:31 +0100

    Restore blog post navigation

--- src/pages/blog/posts/[slug].js
+++ src/pages/blog/posts/[slug].js
@@ -4,18 +4,18 @@ import matter from 'gray-matter'
 import dynamic from 'next/dynamic'
 import Head from 'next/head'
 import Image from 'next/image'
-import { MDXProvider } from '@mdx-js/react'
 import { MDXRemote } from 'next-mdx-remote'
 import { serialize } from 'next-mdx-remote/serialize'
+import rehypeImgSize from 'rehype-img-size'
+import rehypeKatex from 'rehype-katex'
 import remarkMath from 'remark-math'
 import markdown from 'remark-parse'
 import remarkPrism from 'remark-prism'
-import rehypeKatex from 'rehype-katex'
 
 import siteConfig from '../../../config'
-import Layout from '../../../components/layout'
 import Content from '../../../components/content'
-import rehypeImgSize from 'rehype-img-size'
+import Layout from '../../../components/layout'
+import Navigation from '../../../components/navigation'
 
 const components = {
   BilliardsContainer: dynamic(() =>
@@ -30,13 +30,34 @@ export async function getStaticProps({ params: { slug } }) {
   // mix of .md and .mdx files. This isn't 100% robust, but the failure case is
   // when a directory contains file and sub-directory with same name which for
   // the purposes of my personal blog I'm comfortable ignoring
-  const fileName = fs
-    .readdirSync(pagesDirectory)
-    .find((path) => path.startsWith(slug))
-  const fullPath = path.join(pagesDirectory, fileName)
-  const fileContents = fs.readFileSync(fullPath, 'utf8')
+  // const fileName = fs
+  //   .readdirSync(pagesDirectory)
+  //   .find((path) => path.startsWith(slug))
+  // const fullPath = path.join(pagesDirectory, fileName)
+  // const fileContents = fs.readFileSync(fullPath, 'utf8')
 
-  const { content, data } = matter(fileContents)
+  let posts = fs.readdirSync(pagesDirectory).map((fileName) => {
+    const fileContents = fs.readFileSync(
+      path.join(pagesDirectory, fileName),
+      'utf8',
+    )
+    const { data, content } = matter(fileContents)
+    return { slug: fileName.replace(/.mdx?$/, ''), ...data, content }
+  })
+  // sort in reverse chronological order
+  posts = posts.sort((a, b) => (a.date < b.date ? 1 : -1))
+  posts.forEach((post, idx) => {
+    if (idx < posts.length - 1) {
+      post.prev = posts[idx + 1].slug
+      post.prevLabel = posts[idx + 1].title
+    }
+    if (idx > 0) {
+      post.next = posts[idx - 1].slug
+      post.nextLabel = posts[idx - 1].title
+    }
+  })
+
+  const { content, ...data } = posts.find((post) => post.slug === slug)
 
   const mdxSource = await serialize(content, {
     mdxOptions: {
@@ -83,9 +104,13 @@ export default function Page({ slug, source, meta }) {
           excerpt={meta.excerpt}
           path={`/${slug}`}
         >
-          <MDXProvider components={components}>
-            <MDXRemote {...source} />
-          </MDXProvider>
+          <MDXRemote {...source} components={components} />
+          <Navigation
+            nextPath={meta.next && `/blog/posts/${meta.next}`}
+            nextLabel={meta.nextLabel}
+            previousPath={meta.prev && `/blog/posts/${meta.prev}`}
+            previousLabel={meta.prevLabel}
+          />
         </Content>
       </Layout>
     </>
