689f96a93c186b565d31e06ff647caa87b2e69dd
tcbegley <tomcbegley@gmail.com>
2022-09-09 06:54:19 +0100

    clean up unused Gatsby plugins

--- plugins/gatsby-plugin-goatcounter/gatsby-node.js
+++
@@ -1,7 +0,0 @@
-exports.onPreInit = ({ reporter }, options) => {
-  if (!options.code) {
-    reporter.error(
-      `The GoatCounter plugin requires a page code. Did you mean to add it?`
-    );
-  }
-};
