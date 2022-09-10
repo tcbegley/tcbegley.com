25e833fdb42a9cb833489cf07e60399945bd1c1e
tcbegley <tomcbegley@gmail.com>
2022-09-10 16:48:03 +0100

    redirects

--- next.config.js
+++ next.config.js
@@ -3,6 +3,20 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: { domains: ['live.staticflickr.com'] },
+  async redirects() {
+    return [
+      'election-modelling-1',
+      'election-modelling-2',
+      'election-modelling-3',
+      'bayesian-billiards',
+      'mcmc-part-1',
+      'mcmc-part-2',
+    ].map((id) => ({
+      source: `/blog/${id}`,
+      destination: `/blog/posts/${id}`,
+      permanent: true,
+    }))
+  },
 }
 
 module.exports = nextConfig
